b9c09ff495e3a0efdae0155252bd58921b72f806
Jatin Sharma <jatinsharma6697@gmail.com>
2025-06-22 15:33:37 +0530

    Added New Syso Line

--- My_Java_Project_2025/src/mypackage/test2.java
+++ My_Java_Project_2025/src/mypackage/test2.java
@@ -3,8 +3,16 @@ package mypackage;
 public class test2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int number1 = 15;
+        int number2 = 25;
+
+        // Calculate the sum
+        int sum = number1 + number2;
+
+        // Print the result
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+        System.out.println("Number1 and number2 has been added");
+    }
 
 	}
 
-}
